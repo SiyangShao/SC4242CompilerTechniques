7acbad5f86ec2082c9e053822f4424070dac9812
kcz358 <92624596+kcz358@users.noreply.github.com>
2024-09-04 10:01:51 +0800

    Add punc and operator test cases

--- src/test/LexerTests.java
+++ src/test/LexerTests.java
@@ -103,5 +103,43 @@ public class LexerTests {
                 new Token(EOF, 0, 4, ""));
     }
 
+    @Test
+    public void testPunctuationBracket() {
+        runtest("[(), (), ()]; {\"value_1\", \"value_2\"}",
+                new Token(LBRACKET, 0, 0, "["),
+                new Token(LPAREN, 0, 1, "("),
+                new Token(RPAREN, 0, 2, ")"),
+                new Token(COMMA, 0, 3, ","),
+                new Token(LPAREN, 0, 5, "("),
+                new Token(RPAREN, 0, 6, ")"),
+                new Token(COMMA, 0, 7, ","),
+                new Token(LPAREN, 0, 9, "("),
+                new Token(RPAREN, 0, 10, ")"),
+                new Token(RBRACKET, 0, 11, "]"),
+                new Token(SEMICOLON, 0, 12, ";"),
+                new Token(LCURLY, 0, 14, "{"),
+                new Token(STRING_LITERAL, 0, 15, "value_1"),
+                new Token(COMMA, 0, 24, ","),
+                new Token(STRING_LITERAL, 0, 26, "value_2"),
+                new Token(RCURLY, 0, 35, "}"),
+                new Token(EOF, 0, 36, ""));
+    }
+
+    @Test
+    public void testOperators(){
+        runtest("/ == = >= <= > < - + *",
+                new Token(DIV, 0, 0, "/"),
+                new Token(EQEQ, 0, 2, "=="),
+                new Token(EQL, 0, 5, "="),
+                new Token(GEQ, 0, 7, ">="),
+                new Token(LEQ, 0, 10, "<="),
+                new Token(GT, 0, 13, ">"),
+                new Token(LT, 0, 15, "<"),
+                new Token(MINUS, 0, 17, "-"),
+                new Token(PLUS, 0, 19, "+"),
+                new Token(TIMES, 0, 21, "*"),
+                new Token(EOF, 0, 22, ""));
+    }
+
 
 }
